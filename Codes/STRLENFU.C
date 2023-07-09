2aca877b7036f70c8e38d4f8cf2b04bf1d786439
Yash Kolhatkar <138909671+yashkolhatkar09@users.noreply.github.com>
2023-07-09 21:59:48 +0530

    Add files via upload

---
+++ Codes/STRLENFU.C
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include<conio.h>
+ #include<string.h>
+ void main()
+
+ {
+ char str1[30];
+
+ clrscr();
+
+ printf("Enter Your String Here :");
+
+ scanf("%s",str1);
+
+ printf("The Length of string is : %d", strlen(str1));
+
+ getch();
+
+ }
